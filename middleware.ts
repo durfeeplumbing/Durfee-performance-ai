16a69ad8691170bb191f7034174bfa792aa6fe2e
durfeeplumbing <durfeeplumbing@gmail.com>
2026-08-29 21:34:25 -0400

    Add authenticated route pathname middleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export function middleware(request:NextRequest){
+  const headers=new Headers(request.headers);
+  headers.set('x-durfee-pathname',request.nextUrl.pathname);
+  return NextResponse.next({request:{headers}});
+}
+
+export const config={matcher:['/((?!_next/static|_next/image|favicon.ico).*)']};
